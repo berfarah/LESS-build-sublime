6df8ec9883ca7febd5eb796275c15bbb16f62503
jvalc <jsanta@bonline.com>
2013-01-10 18:07:14 +0000

    Update changeLESSBuildType.bat
    Links to User folder

--- changeLESSBuildType.bat
+++ changeLESSBuildType.bat
@@ -3,12 +3,16 @@ cls
 :start
 ECHO.
 ECHO 1. Print Normal
-ECHO 2. Print Directory Rewrite
+ECHO 2. Print Normal User
+ECHO 3. Print Directory Rewrite
+ECHO 4. Print Directory Rewrite User
 
 set /p choice=Type the number to print text.
 if not '%choice%'=='' set choice=%choice:~0,1%
 if '%choice%'=='1' goto normal
-if '%choice%'=='2' goto DirRew
+if '%choice%'=='2' goto normalUser
+if '%choice%'=='3' goto DirRew
+if '%choice%'=='4' goto DirRewUser
 
 ECHO "%choice%" is not valid please try again
 ECHO.
@@ -19,9 +23,17 @@ goto start
 del LESS.sublime-build
 mklink LESS.sublime-build LESS-normal.sublime-build-choice 
 goto end
+:normalUser
+del LESS.sublime-build
+mklink ../User/LESS.sublime-build LESS-normal.sublime-build-choice 
+goto end
 :DirRew
 del LESS.sublime-build
 mklink LESS.sublime-build LESS-rewriteDir.sublime-build-choice 
+goto end
+:DirRewUser
+del LESS.sublime-build
+mklink ../User/LESS.sublime-build LESS-rewriteDir.sublime-build-choice 
 goto end
 :test
 ECHO TEST
