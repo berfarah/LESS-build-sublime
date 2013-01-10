4f21b9ba35803f5023d9d863449da744262307f6
jvalc <jsanta@bonline.com>
2013-01-10 18:04:32 +0000

    Update changeLESSBuildType.sh
    Removing extralines

--- changeLESSBuildType.sh
+++ changeLESSBuildType.sh
@@ -1,64 +1,35 @@
 #!/bin/bash
-
 PS3='Please enter your choice: '
-
 options=("Normal" "Normal-User" "Directory Rewrite" "Directory Rewrite-User" "Quit")
-
 select opt in "${options[@]}"
 
 do
-
     case $opt in
-
         "Normal")
-
             rm LESS.sublime-build
-
             ln -s LESS-normal.sublime-build-choice LESS.sublime-build
-
             break;
-
             ;;
-
         "Normal-User")
-
             rm LESS.sublime-build
-
             ln -s ../User/LESS-normal.sublime-build-choice LESS.sublime-build
-
             break;
-
             ;;
-
         "Directory Rewrite")
-
             rm LESS.sublime-build
-
             ln -s LESS-rewriteDir.sublime-build-choice LESS.sublime-build
-
             break;
-
             ;;
-
         "Directory Rewrite-User")
-
             rm LESS.sublime-build
-
             ln -s ../User/LESS-rewriteDir.sublime-build-choice LESS.sublime-build
-
             break;
-
             ;;
 
         "Quit")
-
             break
-
             ;;
-
         *) echo invalid option;;
-
     esac
-
 done
 
